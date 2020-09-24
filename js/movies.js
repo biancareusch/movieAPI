a8a853335d86c2b1f5f061ad40edf9f1ccca19c6
Bianca Reusch <reusch.bianca@gmail.com>
2020-09-24 10:13:41 -0500

    can grab movie id with delete button

--- js/movies.js
+++ js/movies.js
@@ -1,62 +1,70 @@
-(function (){
+(function () {
 
 
-
-const URL = "https://capable-zenith-layer.glitch.me/movies";
+    const URL = "https://capable-zenith-layer.glitch.me/movies";
 //fetch all movies on server
-fetch(URL)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        data.forEach(movies => {
-            $('.movies').append(`<div class="movieDiv">${movies.title} ${movies.rating} <button class='deleteMovie'>X</button></div>`)
-        });
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            data.forEach(movies => {
+                $('.movies').append(`<div class="movieDiv">${movies.title} ${movies.rating} <button class='deleteMovie' type="submit" data-id=“${movies.id}”>X</button>`)
+            });
 
 //add movies
-        $('.addMovie').click((e) => {
-            e.preventDefault()
-            let inputText = $("#addInput").val();
-            let inputRating = $(".addRating").val();
+            $('.addMovie').click((e) => {
+                e.preventDefault()
+                let inputText = $("#addInput").val();
+                let inputRating = $(".addRating").val();
 //new movie object
-            let movieObj = {
-                title: inputText,
-                rating: inputRating
-            };
+                let movieObj = {
+                    title: inputText,
+                    rating: inputRating
+                };
 //adding it to server data
-            let options = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(movieObj),
-            };
+                let options = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(movieObj),
+                };
 
-            fetch(URL, options)
-                .then(response => response.json())
-                .catch(error => console.log(error))
-            $('.movies').append("<div>" + $("#addInput").val() + " " + $(".addRating").val() + "<button class='deleteMovie'>X</button>" + "</div>");
-        })
-
-        $('.deleteMovie').click(e => {
-            e.preventDefault()
-            console.log(this.getElementsByClassName("movieDiv"));
-            console.log(this.parentElement)
-        })
-
-        // console.log(document.getElementsByClassName("movieDiv"));
+                fetch(URL, options)
+                    .then(response => response.json())
+                    .catch(error => console.log(error))
+                $('.movies').append("<div>" + $("#addInput").val() + " " + $(".addRating").val() + `<button  type="submit" class='deleteMovie'>X</button>` + "</div>");
+            })
 
 
-        //delete movies by ID
-        function deleteMovie (id) {
-            fetch(`${URL}/${id}`,
-                {method: 'DELETE'})
-        }
+            //delete movies by ID
+            function deleteMovie(id) {
+                fetch(URL +'/' + id, {method: 'DELETE'})
+                    .then(data => data.json)
+                    console.log(id);
+            }
 
-
-    });
+$(document).on("click", ".deleteMovie", function(e) {
+    e.preventDefault();
+    console.log("getting here");
+    console.log($(this).data("id"));
+    let newid = $(this).data("id");
+    deleteMovie(newid);
+})
 
 
 
+        });
+
+
+
+
+//EDIT
+//     $(document).on("click", ".edit", function(e){
+//         e.preventDefault();
+//         console.log("something");
+//         let editID = $(this).data("id");
+//         console.log(editID);
 
 
 // IIFE don't touch
